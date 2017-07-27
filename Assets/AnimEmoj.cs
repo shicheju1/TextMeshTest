1402c3f33d14d2b87f104a0a020f58d9993bbae9
laijingfeng <337292616@qq.com>
2017-07-27 21:55:39 +0800

    update

---
+++ Assets/AnimEmoj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class AnimEmoj : MonoBehaviour
+{
+    private TextMeshProUGUI txt;
+
+    void Start()
+    {
+        txt = this.GetComponent<TextMeshProUGUI>();
+        this.StartCoroutine(IE_Anim());
+    }
+
+    private IEnumerator IE_Anim()
+    {
+        int[] arr = new int[] { 0, 1, 2, 3, 4 };
+        int idx = 0;
+        while (true)
+        {
+            txt.text = string.Format("hello<sprite={0}><size=200%><sprite=2></size>world", arr[idx]);
+            idx = (idx + 1) % arr.Length;
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
